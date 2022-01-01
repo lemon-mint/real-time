fd415ec51cd5cce3ec5859e152e2fadb9de0f15c
lemon-mint <ice.lemons.mint@gmail.com>
2022-01-01 21:05:28 +0900

    try catch

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ let errorString: string = "";
 let DOMTimeOff: number = new Date().getTime() - performance.now();
 let displayTime: string = new Date().toLocaleString();
 
-let statusText: string = "잠시 기다려주세요, 동기화 중입니다.";
+let statusText: string = "Please wait, synchronizing...";
 
 function getCurrentTime() {
   return performance.now() + DOMTimeOff + timeOffset;
@@ -58,34 +58,57 @@ async function sleep(t: number) {
   return new Promise((resolve) => setTimeout(resolve, t));
 }
 
-async function syncTime(repeat: boolean = false) {
+async function syncTime(repeat: boolean = false, multiplier: number = 1.0) {
   await getServerOff(); // Preheat
 
   let offset = 0;
   const TIME_SYNC_MEASURE_COUNT = 10;
-  for (let i = 0; i < TIME_SYNC_MEASURE_COUNT; i++) {
-    offset += await getServerOff();
-    statusText = "동기화중... " + (offset/(i+1)).toFixed(5) + "ms";
-    await sleep(25);
-  }
-  offset /= TIME_SYNC_MEASURE_COUNT;
 
-  console.log("offset: " + offset);
+  for (let i = 0; i < TIME_SYNC_MEASURE_COUNT; i++) {
+    console.log("Measurement: " + i);
+    console.log("Requesting time offset...");
+    const off = await getServerOff();
+    offset += off;
+    console.log("Server offset: " + off);
+
+    statusText = "Synchronizing... " + (offset / (i + 1)).toFixed(5) + "ms";
+    await sleep(20);
+  }
+
+  offset /= TIME_SYNC_MEASURE_COUNT;
+  offset *= multiplier;
+
+  console.log("Synchronized time offset: " + offset);
   setTimeOffset(offset);
   statusText = "" + (getCurrentTime() - new Date().getTime()).toFixed(5) + "ms";
   if (repeat) {
     setTimeout(async () => {
-      await syncTime(true);
+      await syncTime(true, multiplier);
     }, 1000 * 30);
+    console.log(
+      "Next Synchronization is scheduled on " +
+        new Date(getCurrentTime() + 1000 * 30).toLocaleString()
+    );
   }
 }
 
 async function main() {
-  await getServerOff();
-  await sleep(500);
-  await syncTime(false);
-  await sleep(1000);
-  await syncTime(true);
+  try {
+    errorString = "";
+    statusText = "Please wait, synchronizing...";
+    console.log("Starting synchronization...");
+    await getServerOff();
+    await sleep(500);
+    await syncTime(false);
+    await sleep(100);
+    await syncTime(true, 0.9);
+    console.log("Successfully synchronized time.");
+  } catch (e) {
+    console.error(e);
+    errorString = e.toString();
+    statusText = "Error occurred. Restarting in 5 seconds.";
+    setTimeout(main, 5000);
+  }
 }
 
 main().then();
