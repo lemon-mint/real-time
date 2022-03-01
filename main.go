57646d30643ff30d054b6f3ccd1435b4cb43f728
lemon-mint <ice.lemons.mint@gmail.com>
2022-03-01 16:06:36 +0900

    private

--- main.go
+++ main.go
@@ -49,9 +49,8 @@ func getBadgeURL(label, message, color, style string) string {
 func badgeHandle(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "image/svg+xml")
 	// No Cache
-	w.Header().Set("Cache-Control", "no-cache, no-store, must-revalidate")
+	w.Header().Set("Cache-Control", "private, no-cache, no-store, must-revalidate")
 	w.Header().Set("Pragma", "no-cache")
-	w.Header().Set("Expires", "0")
 
 	version := r.URL.Query().Get("version")
 	if version == "" {
